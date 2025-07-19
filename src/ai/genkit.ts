62110d4cdae3187c4c9a01302f6d0974f816f387
Sachin Srivastava <sachinsrivastava.id@gmail.com>
2025-07-19 13:28:14 +0000

    Console Error Server
    TypeError: (0 , _genkit_ai_next__WEBPACK_IMPORTED

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,6 @@ import {googleAI} from '@genkit-ai/googleai';
 import {nextPlugin} from '@genkit-ai/next';
 
 export const ai = genkit({
-  plugins: [googleAI(), nextPlugin()],
+  plugins: [googleAI(), nextPlugin],
   model: 'googleai/gemini-1.5-pro-latest',
 });
